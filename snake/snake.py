95b3339754fd6a2724271f80e05d0c531f19d011
Frankataccess <frank.mccann.355@accesscreative.ac.uk>
2023-12-02 10:41:19 +0000

    attempted score

--- snake/snake.py
+++ snake/snake.py
@@ -128,23 +128,29 @@ class MAIN:
         self.draw_grass()
         self.fruit.draw_fruit()
         self.snake.draw_snake()
+        self.draw_score()
 
     def check_collision(self):
         if self.fruit.pos == self.snake.body[0]:
             self.fruit.randomize()
             self.snake.add_block()
-    
+            self.snake.play_crunch_sound()
+
+        for block in self.snake.body[1:]:
+            if block == self.fruit.pos:
+                self.fruit.randomize()
+
     def check_fail(self):
         if not 0 <= self.snake.body[0].x < cell_number or not 0 <= self.snake.body[0].y < cell_number:
-            self.game_over( )
-        
+            self.game_over()
+
         for block in self.snake.body[1:]:
             if block == self.snake.body[0]:
                 self.game_over()
-
+        
     def game_over(self):
-            pygame.quit()
-            sys.exit()
+        self.snake.reset()
+
 
     def draw_grass(self):
         grass_color = (167,209,61)
@@ -159,6 +165,14 @@ class MAIN:
                     if col % 2 != 0:
                         grass_rect = pygame.Rect(col * cell_size,row * cell_size,cell_size,cell_size)
                         pygame.draw.rect(screen,grass_color,grass_rect)			
+    
+
+    def draw_score(self):
+        score_text = str(len(self.snake.body) - 3)
+        score_surface = game_font.render(score_text,True,(56,74,12))
+        score_x = int(cell_size * cell_number - 60)
+        score_y = int(cell_size * cell_number - 40)
+        score_rect = score_surface.get_rect(center = (score_x,score_y))
 
 pygame.init()
 cell_size = 40
@@ -166,6 +180,7 @@ cell_number = 20
 screen = pygame.display.set_mode((cell_number*cell_size,cell_number*cell_size))
 clock = pygame.time.Clock()
 apple = pygame.image.load('snake/graphics/apple.png').convert_alpha()
+game_font = pygame.font.Font('snake/font/PoetsenOne-Regular.ttf', 25)
 
 fruit = FRUIT()
 snake = SNAKE()
